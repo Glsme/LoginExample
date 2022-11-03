29da6831f9138e5b2d29836ab2daf85718f30008
Glsme <glasses.str.man@gmail.com>
2022-11-04 02:36:28 +0900

    Feat: API Error LocalizedError Extension 코드 추가

--- LoginExample/Domain/Network/Model/APIError.swift
+++ LoginExample/Domain/Network/Model/APIError.swift
@@ -13,3 +13,18 @@ enum APIError: Error {
     case responseError
     case alreadyExist
 }
+
+extension APIError: LocalizedError {
+    var errorDescription: String? {
+        switch self {
+        case .noData:
+            return "데이터가 없습니다."
+        case .errorInParsingData:
+            return "데이터 파싱 과정 중 에러가 발생하였습니다."
+        case .responseError:
+            return "Reponse Error: 상태 코드를 체크해주세요"
+        case .alreadyExist:
+            return "이미 계정이 존재합니다."
+        }
+    }
+}
